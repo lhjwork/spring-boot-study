b6088beb4883a00bd3c42fcf004e52e318ae587b
hanjin lee <hanjinlee@hanjinui-MacBookPro.local>
2026-04-13 20:17:43 +0900

    상품목록 페이지 만들기 (Thymeleaf)

---
+++ src/main/java/com/mib/shop/BasicController.java
@@ -0,0 +1,14 @@
+package com.mib.shop;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class BasicController {
+
+
+    @GetMapping("/")
+    String main(){
+        return "index.html";
+    }
+}
