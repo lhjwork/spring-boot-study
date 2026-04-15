533ad4224706b539d269c67d1cb17d22d1f9a45b
hanjin lee <hanjinlee@hanjinui-MacBookPro.local>
2026-04-15 23:18:33 +0900

    jpa로 데이터 입출력 - 1. repository create, 2. apply for repository to want a class, 3. repository.입출력문법

--- src/main/java/com/mib/shop/ItemController.java
+++ src/main/java/com/mib/shop/ItemController.java
@@ -1,14 +1,26 @@
 package com.mib.shop;
 
+import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 
+import java.util.List;
+
 @Controller
+@RequiredArgsConstructor
 public class ItemController {
+
+   private final ItemRepository itemRepository;
 
     @GetMapping("/list")
     String list(Model model) {
+
+        List<Item> result = itemRepository.findAll();
+        System.out.println(result.get(0).title);
+        System.out.println(result.get(0).price);
+
+        System.out.println(result);
         model.addAttribute("name", "홍길동");
         return "list.html";
     }
