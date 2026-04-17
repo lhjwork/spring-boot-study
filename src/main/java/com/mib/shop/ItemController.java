15b5351a2d5b35ef045de47a739a387de828c000
hanjin lee <hanjinlee@hanjinui-MacBookPro.local>
2026-04-17 22:43:10 +0900

    상품 추가기능 1 (Map 자료형)

--- src/main/java/com/mib/shop/ItemController.java
+++ src/main/java/com/mib/shop/ItemController.java
@@ -1,10 +1,14 @@
 package com.mib.shop;
 
 import lombok.RequiredArgsConstructor;
+import java.util.Map;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 
+import java.util.HashMap;
 import java.util.List;
 
 @Controller
@@ -20,5 +24,27 @@ public class ItemController {
         System.out.println(a.toString());
         model.addAttribute("items",  result );
         return "list.html";
+    }
+
+    @GetMapping("/write")
+    String write() {
+        return "write.html";
+    }
+
+    @PostMapping("/items")
+    String addItem(@RequestParam String title, @RequestParam String price){
+        System.out.println(title);
+        System.out.println(price);
+        return "redirect:/list";
+    }
+
+    @PostMapping("/items/map")
+    String addMapItem (@RequestParam Map formData){
+        HashMap<String, Object> test = new HashMap<>();
+        test.put("name","kim");
+        test.put("age",20);
+        System.out.println(test);
+
+        return "redirect:/list";
     }
 }
